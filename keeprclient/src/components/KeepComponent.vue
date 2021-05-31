02ee7266d9bf087348653b8b8d197f219e64dc71
Michaela Bickish <mmbickish@gmail.com>
2021-05-31 11:29:43 -0600

    small client change

---
+++ keeprclient/src/components/KeepComponent.vue
@@ -0,0 +1,33 @@
+<template>
+  <!--  -->
+  <div class="col-12 col-md-3">
+    card start -->
+    <div class="card text-white m-2 shadow">
+      <img src="https://media-cdn.tripadvisor.com/media/photo-s/02/25/67/df/filename-homer-jpg-thumbnail0.jpg" class="card-img" alt="...">
+      <div class="card-img-overlay d-flex align-items-end justify-content-between">
+        <h5 class="card-title mb-0">
+          Card title
+        </h5>
+        <p class="card-text">
+          Creator Info
+        </p>
+      </div>
+    </div> -->
+  <!-- End card -->
+  </div>
+  <!--  -->
+</template>
+
+<script>
+export default {
+  name: 'KeepComponent',
+  setup() {
+    return {}
+  },
+  components: {}
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
