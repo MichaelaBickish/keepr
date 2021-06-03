1882f57317152ddae0974825eea75a3c86d0cccc
Michaela Bickish <mmbickish@gmail.com>
2021-06-03 12:35:12 -0600

    Getting Vault data on vault page

--- keeprclient/src/pages/VaultDetailsPage.vue
+++ keeprclient/src/pages/VaultDetailsPage.vue
@@ -1,17 +1,42 @@
 <template>
-  <div class="component">
-    Vaults Page
+  <div class="container-fluid" v-if="state.activeVault">
+    <div class="row my-3 mx-2">
+      <div class="col-12">
+        <h1>
+          {{ state.activeVault.name }}
+        </h1>
+        <p class="font-weight-bold text-secondary">
+          Keeps:
+        </p>
+      </div>
+    </div>
+    {{ state.activeVault }}
   </div>
 </template>
 
 <script>
+import { computed, onMounted, reactive } from 'vue'
 import { useRoute } from 'vue-router'
+import { vaultsService } from '../services/VaultsService'
+import Notification from '../utils/Notification'
+import { AppState } from '../AppState'
 export default {
   name: 'VaultDetailsPage',
   setup() {
     const route = useRoute()
+    const state = reactive({
+      activeVault: computed(() => AppState.activeVault)
+    })
+    onMounted(async() => {
+      try {
+        await vaultsService.getVaultById(route.params.id)
+      } catch (error) {
+        Notification.toast('Error: ' + error, 'error')
+      }
+    })
     return {
-      route
+      route,
+      state
     }
   },
   components: {}
--- keeprclient/src/services/VaultsService.js
+++ keeprclient/src/services/VaultsService.js
@@ -5,6 +5,11 @@ class VaultsService {
     const res = await api.post('api/vaults', newVault)
     AppState.vaults.push(res.data)
   }
+
+  async getVaultById(id) {
+    const res = await api.get('api/vaults/' + id)
+    AppState.activeVault = res.data
+  }
 }
 
 export const vaultsService = new VaultsService()
